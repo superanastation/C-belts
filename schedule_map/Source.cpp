3657e32511e8ff38c41ac8ac17702a5c1be86a19
anastasia <kortukovaa@mail.ru>
2019-12-02 17:52:09 +0300

    июль-август 31 день

--- schedule_map/Source.cpp
+++ schedule_map/Source.cpp
@@ -13,7 +13,8 @@ void make_full(int num_d, vector<vector<string>>& routine) {
 
 void query(int n) {
 	vector<vector<string>> routine;
-	int k = 1;//номер текущего мес€ца
+	vector<int> year = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int k = 0;//номер текущего мес€ца
 	make_full(31, routine);
 	for (int i = 0; i < n; i++) {
 		string command;
@@ -26,7 +27,7 @@ void query(int n) {
 		}
 		else if (command == "DUMP") {
 			cin >> num;
-			cout << routine[num].size() << ' ';;
+			cout << routine[num].size() << ' ';
 			for (int j = 0; j < routine[num].size(); j++) {
 				cout << routine[num][j] << ' ';
 			}
@@ -36,37 +37,25 @@ void query(int n) {
 			vector<vector<string>> temp;
 			k+=1;
 			if (k > 11) {
-				k %= 11;
-			}				
-			if (k == 2)
-				make_full(28,temp);
-			else if (k%2==1)
-				make_full(31, temp);
-			else
-				make_full(30, temp);
+				k =0;
+			}
+			make_full(year[k], temp);
 			if (temp.size() >= routine.size()) {
 				for (int j = 0; j < routine.size(); j++) {
 					temp[j] = routine[j];
 				}
 			}
 			else {
-				//temp.insert(end(temp), begin(routine), end(routine));
 				for (int j = 0; j < routine.size(); j++) {
 					if (j < temp.size()) {
 						temp[j] = routine[j];
 					}
 					else {
-						/*for (int m = 0; m < routine[j].size(); m++) {
-							temp[temp.size() - 1][temp[j].size() - 1 + m] = routine[j][m];
-						}*/
 						(temp[temp.size() - 1]).insert(end(temp[temp.size() - 1]),begin(routine[j]), end(routine[j]));
-						//(temp[temp.size()-1]).push_back(routine[j]);
-						//temp.insert(end(temp), begin(routine), end(routine));
-						//(temp.end()).push_back(routine[j]);
 					}
 				}
 			}
-			routine.clear();
+			//routine.clear();
 			routine = temp;
 		}
 			
