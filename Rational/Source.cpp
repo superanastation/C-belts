63f3c39afea192236d4ce5940b74e874f89b3730
anastasia <kortukovaa@mail.ru>
2019-12-10 17:26:14 +0300

    input/output

--- Rational/Source.cpp
+++ Rational/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<sstream>
 using namespace std;
 
 class Rational {
@@ -9,7 +10,7 @@ public:
 	}
 
 	Rational(int numerator, int denominator) {
-		if (numerator == 0) {
+		if (numerator == 0 || denominator==0) {
 			num = numerator;
 			den = 1;
 		}
@@ -65,31 +66,74 @@ public:
 		int new_den = this->den*right.num;
 		return Rational(new_num, new_den);
 	}
+	//istringstream& operator >>(istringstream& stream,Rational& new_r) {
+	//	stream >> this->num;
+	//	stream.ignore(1);
+	//	stream >> this->den;
+	//	return stream;
+	//}
+	//ostringstream& operator <<(ostringstream& stream) {
+	//	stream << this->num;
+	//	stream << '/';
+	//	stream << this->den;
+	//	return stream;
+	//}
 private:
 	int num;
 	int den;
 };
-
+istream& operator >>(istream& stream, Rational& new_r) {
+	int num, den;
+	char check;
+	stream >> num;
+	stream >> check;
+	stream >> den;
+	if (check=='/')
+		new_r = Rational(num,den);
+	return stream;
+}
+ostream& operator << (ostream& str, const Rational& val) {
+	str << val.Numerator() << '/' << val.Denominator();
+	//cout << val.p << '/' << val.q << endl;
+	return str;
+}
 int main() {
 	{
-		Rational a(2, 3);
-		Rational b(4, 3);
-		Rational c = a * b;
-		bool equal = c == Rational(8, 9);
-		if (!equal) {
-			cout << "2/3 * 4/3 != 8/9" << endl;
+		ostringstream output;
+		output << Rational(-6, 8);
+		if (output.str() != "-3/4") {
+			cout << "Rational(-6, 8) should be written as \"-3/4\"" << endl;
 			return 1;
 		}
 	}
 
 	{
-		Rational a(5, 4);
-		Rational b(15, 8);
-		Rational c = a / b;
-		bool equal = c == Rational(2, 3);
+		istringstream input("5/7");
+		Rational r;
+		input >> r;
+		bool equal = r == Rational(5, 7);
 		if (!equal) {
-			cout << "5/4 / 15/8 != 2/3" << endl;
+			cout << "5/7 is incorrectly read as " << r << endl;
 			return 2;
+		}
+	}
+
+	{
+		istringstream input("5/7 10/8");
+		Rational r1, r2;
+		input >> r1 >> r2;
+		bool correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+		if (!correct) {
+			cout << "Multiple values are read incorrectly: " << r1 << " " << r2 << endl;
+			return 3;
+		}
+
+		input >> r1;
+		input >> r2;
+		correct = r1 == Rational(5, 7) && r2 == Rational(5, 4);
+		if (!correct) {
+			cout << "Read from empty stream shouldn't change arguments: " << r1 << " " << r2 << endl;
+			return 4;
 		}
 	}
 
