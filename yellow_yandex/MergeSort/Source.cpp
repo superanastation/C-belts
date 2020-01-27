421f2a4087f7d4edcfea8ee98dc3415048dc157d
anastasia <kortukovaa@mail.ru>
2020-01-27 11:35:20 +0300

    mergesort разбиение на 2 части

---
+++ yellow_yandex/MergeSort/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end) {
+	if (range_end - range_begin < 2)
+		return;
+	vector<typename RandomIt::value_type> elements(range_begin, range_end);
+	RandomIt mid = begin(elements) + elements.size() / 2;
+	MergeSort(begin(elements), mid);
+	MergeSort(mid, end(elements));
+	merge(begin(elements), mid, mid, end(elements), range_begin);
+}
+
+int main() {
+	vector<int> v = { 6, 4, 7, 6, 4, 4, 0, 1 };
+	MergeSort(begin(v), end(v));
+	for (int x : v) {
+		cout << x << " ";
+	}
+	cout << endl;
+	return 0;
+}
--- yellow_yandex/RemoveDuplicates/Source.cpp
+++ yellow_yandex/RemoveDuplicates/Source.cpp
@@ -1,27 +1,40 @@
 #include<string>
 #include<vector>
 #include<iostream>
+#include<algorithm>
+#include<map>
 
 using namespace std;
 
 template <typename T>
 void RemoveDuplicates(vector<T>& elements) {
+	map<T, int> element_count;
+	auto it = remove_if(elements.begin(), elements.end(), [&element_count](T element) mutable {
+		++element_count[element];
+		return element_count[element] > 1;
+	});
+	elements.erase(it, elements.end());
+
 	//vector<T> copy = elements;
-	auto it = begin(elements);
-	while (it != end(elements)) {
-		auto j_it = it+1;
-		while (j_it != end(elements))
-		{
-			if (*it == *j_it) {
-				elements.erase(j_it);
-				RemoveDuplicates(elements);
-				break;
-			}
-			else
-				++j_it;
-		}
-		++it;
-	}
+	//auto it = begin(elements);
+	//while (it != end(elements)) {
+	//	auto j_it = it+1;
+	//	auto new_end = remove_if(j_it, end(elements), [](T& item) {
+	//		return item == it;
+	//	});
+	//	elements.erase(new_end, end(elements));
+	//	//while (j_it != end(elements))
+	//	//{
+	//	//	if (*it == *j_it) {
+	//	//		elements.erase(j_it);
+	//	//		RemoveDuplicates(elements);
+	//	//		break;
+	//	//	}
+	//	//	else
+	//	//		++j_it;
+	//	//}
+	//	++it;
+	//}
 	//for (int i = 0; i < elements.size(); i++) {
 	//	for (int j = i++; j < elements.size(); j++) {
 	//		if (elements[i] == elements[j]) {
@@ -31,7 +44,7 @@ void RemoveDuplicates(vector<T>& elements) {
 	//		}
 	//	}
 	//}
-	return;
+	//return;
 }
 
 int main() {
