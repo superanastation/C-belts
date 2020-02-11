2af61294ad594534b37d09e5a6a6212da3cf394d
anastasia <kortukovaa@mail.ru>
2020-02-11 11:17:27 +0300

    решение через deque

--- yellow_yandex/Expression/Source.cpp
+++ yellow_yandex/Expression/Source.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include<string>
+#include<deque>
 
 using namespace std;
+
+struct PartExp {
+	char operation;
+	int number;
+};
 
 int main() {
 	string out;
@@ -9,21 +15,29 @@ int main() {
 	cin >> num;
 	int N;
 	cin >> N;
-	out = to_string(num);
+	deque<PartExp> deq;
+
 	if (N > 0)
 	{
-		out = "(" + out + ")";
-
 		for (int i = 0; i < N; i++) {
 			char op;
 			int r_num;
 			cin >> op >> r_num;
-			out = out + " " + op + " " + to_string(r_num);
-			if (i != N - 1)
-				out = "(" + out + ")";
+			PartExp in = { op,r_num };
+			deq.push_back(in);
 		}
+		for (int i = 0; i < N; i++) {
+			cout << "(";
+		}
+		cout << to_string(num) << ") ";
+		for (int i = 0; i < N - 1; i++) {
+			cout << deq.at(i).operation << " " << to_string(deq.at(i).number) << ") ";
+		}
+		cout << deq.at(N - 1).operation << " " << to_string(deq.at(N - 1).number);
 	}
-	
-	cout << out << endl;
+	else {
+		out = to_string(num);
+		cout << out << endl;
+	}
 	return 0;
 }
