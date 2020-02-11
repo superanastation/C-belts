872658434c8bdf88e6394b7282b58f14e6c18d6c
anastasia <kortukovaa@mail.ru>
2020-02-11 16:39:48 +0300

    промежуточная точка

--- yellow_yandex/Expression/Source.cpp
+++ yellow_yandex/Expression/Source.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<string>
 #include<deque>
+#include<stack>
+#include<sstream>
 
 using namespace std;
 
@@ -15,7 +17,7 @@ int main() {
 	cin >> num;
 	int N;
 	cin >> N;
-	deque<PartExp> deq;
+	stack<PartExp> deq;
 
 	if (N > 0)
 	{
@@ -24,16 +26,28 @@ int main() {
 			int r_num;
 			cin >> op >> r_num;
 			PartExp in = { op,r_num };
-			deq.push_back(in);
+			deq.push(in);
 		}
+		ostringstream s_out;
+		auto it = deq.top();
+		bool pre_priority = false;
 		for (int i = 0; i < N; i++) {
+			auto last = deq.top();
+			deq.pop();
+			auto pre_last = deq.top();
+			if ((pre_last.operation == '+' || pre_last.operation == '-') && (last.operation == '*' || last.operation == '/'))
+			{
+
+			}
+		}
+		/*for (int i = 0; i < N; i++) {
 			cout << "(";
 		}
 		cout << to_string(num) << ") ";
 		for (int i = 0; i < N - 1; i++) {
 			cout << deq.at(i).operation << " " << to_string(deq.at(i).number) << ") ";
 		}
-		cout << deq.at(N - 1).operation << " " << to_string(deq.at(N - 1).number);
+		cout << deq.at(N - 1).operation << " " << to_string(deq.at(N - 1).number);*/
 	}
 	else {
 		out = to_string(num);
