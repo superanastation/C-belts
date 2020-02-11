f1afdd88c3b1325a0399822815a93bf53a61aa7c
anastasia <kortukovaa@mail.ru>
2020-02-11 16:47:49 +0300

    решение второй части через сумму строк

--- yellow_yandex/Expression/Source.cpp
+++ yellow_yandex/Expression/Source.cpp
@@ -12,15 +12,21 @@ int main() {
 	out = to_string(num);
 	if (N > 0)
 	{
-		out = "(" + out + ")";
-
+		//out = "(" + out + ")";
+		char pre_op = '*';
 		for (int i = 0; i < N; i++) {
 			char op;
 			int r_num;
 			cin >> op >> r_num;
-			out = out + " " + op + " " + to_string(r_num);
-			if (i != N - 1)
-				out = "(" + out + ")";
+			if ((pre_op == '+' || pre_op == '-') && (op == '*' || op == '/'))
+			{
+				out = "(" + out + ")" + " " + op + " " + to_string(r_num);
+			}
+			else
+				out = out + " " + op + " " + to_string(r_num);
+			pre_op = op;
+			//if (i != N - 1)
+			//	out = "(" + out + ")";
 		}
 	}
 	
